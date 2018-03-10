39723756839c2ae929840c2b976f9d6fb28423d1
Daniel John Benton <danieljbwork@gmail.com>
2018-03-10 23:03:55 +0000

    note about "case_insensitive"

--- copy_meta/background.js
+++ copy_meta/background.js
@@ -1,6 +1,9 @@
 /*
 	Example link:
 	https://books.google.com/ngrams/graph?content=a%2Cb%2Cc&year_start=1800&year_end=2000&corpus=15&smoothing=3&share=&direct_url=t1%3B%2Ca%3B%2Cc0%3B.t1%3B%2Cb%3B%2Cc0%3B.t1%3B%2Cc%3B%2Cc0
+	
+	Note - case INsensitivity is either "on" or absent:
+	https://books.google.com/ngrams/graph?content=a%2Cb%2Cc&case_insensitive=on&year_start=1800&year_end=2000&corpus=15&smoothing=3&share=&direct_url=t4%3B%2Ca%3B%2Cc0%3B%2Cs0%3B%3Ba%3B%2Cc0%3B%3BA%3B%2Cc0%3B.t4%3B%2Cb%3B%2Cc0%3B%2Cs0%3B%3BB%3B%2Cc0%3B%3Bb%3B%2Cc0%3B.t4%3B%2Cc%3B%2Cc0%3B%2Cs0%3B%3BC%3B%2Cc0%3B%3Bc%3B%2Cc0
 */
 function copyTabUrl(tabUrl)
 {
